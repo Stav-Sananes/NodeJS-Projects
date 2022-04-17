4015cff7e2a49e712c362e0d58b49f9f0c7e4c36
Stav Naftali Sananes <77387159+Stav-Sananes@users.noreply.github.com>
2022-04-17 13:45:04 +0300

    Update products.js

--- Store-express/controllers/products.js
+++ Store-express/controllers/products.js
@@ -32,6 +32,21 @@ const getAllProducts = async (req, res) => {
   } else {
     result = result.sort('createAt');
   }
+  const operatorMap = {
+    '>' : '$gt',
+     '>=' : '$gte',
+     '=' : '$eq',
+     '<' : '$lt',
+     '<=' : '$lte',
+  }
+  const regEx = /\b(<|>|>=|=|<|<=)\b/g
+  let filters = nummericFilter.replace(regEx,(match) =>`-${operatorMap[match]}-`)
+  const options = ['price','rating'];
+  filters = filters.split(',').forEach((item)=>{
+  const [field,operator,value] = item.split('-')
+  if(options.includes(field){
+     queryObject[field] = {[operator]:Number(value)}
+  })
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
