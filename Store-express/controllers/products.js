d7374e9f9de07d76cb63bb2f22ffaa1af829ebf3
Stav Naftali Sananes <77387159+Stav-Sananes@users.noreply.github.com>
2022-04-17 13:47:10 +0300

    Update products.js
    Adding comments to the code.

--- Store-express/controllers/products.js
+++ Store-express/controllers/products.js
@@ -5,16 +5,18 @@ const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({}).sort('name price').limit(5).skip(2);
   res.status(200).json({ products, nbHits: products.length });
 };
-
+ // מקבל את כל הנתונים מהמסמך
 const getAllProducts = async (req, res) => {
   const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
+  // מחפש את החברה
   if (company) {
     queryObject.company = company;
   }
+   // מחפש את השם של המוצר
   if (name) {
     queryObject.name = name;
   }
@@ -32,6 +34,7 @@ const getAllProducts = async (req, res) => {
   } else {
     result = result.sort('createAt');
   }
+  // מתבצע REGEX 
   const operatorMap = {
     '>' : '$gt',
      '>=' : '$gte',
@@ -47,14 +50,16 @@ const getAllProducts = async (req, res) => {
   if(options.includes(field){
      queryObject[field] = {[operator]:Number(value)}
   })
+  // איזה דף מגיע ה שאילתה
   const page = Number(req.query.page) || 1;
+  // כמה נתונים נקבל בדף אחד 
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
   result = result.skip(skip).limit(limit);
   const products = await result;
   res.status(200).json({ products });
 };
-
+// מוציא את הפונקציות שיהיה אפשר להשתמש בהם גם מחוץ לקונטרולר
 module.exports = {
   getAllProducts,
   getAllProductsStatic,
